027ff178bed4bdf379359a10113e69e053fe906a
stasDondov <72253108+stasDondov@users.noreply.github.com>
2022-02-12 18:19:27 +0200

    Add files via upload

---
+++ Task_1.m
@@ -0,0 +1,31 @@
+disp("Task 1, a)")
+
+eps = 10^(-8);
+n = 1;
+y_0 = 1/8;
+y_1 = 0;
+difference = eps + 1;
+while abs(difference) > eps
+  n = n + 1;
+  y_0 = (y_0 * (n^3) + n^2) / (n+1)^3;
+  difference = y_0 - y_1;
+  y_1 = y_0;
+endwhile
+
+printf("Limit: %.2f\n\n", y_1);
+
+
+disp("Task 1, b)")
+eps = 10^(-8);
+n = 1;
+y_0 = 1/2;
+difference = y_0 - y_1;
+y_1 = y_0;
+while abs(difference) > eps
+  n = n + 1;
+  y_0 = y_0 + 1/(n*(n+1));
+  difference = y_0 - y_1;
+  y_1 = y_0;
+endwhile
+
+printf("Limit: %.2f\n", y_1);
---
+++ Task_2.m
@@ -0,0 +1,51 @@
+disp("Task 2, a)")
+
+x = 0;
+sup = 0;
+inf = Inf;
+difference = 1;
+n = 0;
+Max_iter = 500;
+while difference <= Max_iter
+  xn = (1 + 2*(-1)^n)/(3+(-1)^n);
+  if (xn > sup)
+    sup = xn;
+  endif
+  if (xn<inf)
+    inf = xn;
+  endif
+  difference = difference + 1;
+  n++;
+endwhile
+
+printf("Max iter. = %d reached\n", Max_iter);
+printf("Верхня границя: %.2f\n", sup);
+printf("Нижня границя: %.2f\n\n", inf);
+
+
+disp("Task 2, b)");
+
+eps = 10^(-5);
+x = 0;
+sup = 0;
+inf = Inf;
+difference = 1;
+n = 0;
+Max_iter = 500;
+while abs(difference) > eps
+  xn = (-1)^n*((n+1)/(n+2))+sin((pi*n)/2);
+  if (xn > sup)
+    sup = xn;
+  endif
+  if (xn < inf)
+    inf = xn;
+  endif
+  difference = xn - x;
+  if n > Max_iter
+    printf("Max iter. = %d reached\n", Max_iter);
+    break
+  endif
+  n++;
+endwhile
+printf("Верхня границя: %.2f\n", sup);
+printf("Нижня границя: %.2f\n", inf);
